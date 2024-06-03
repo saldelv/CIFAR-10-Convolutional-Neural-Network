5488aaf966fa76975808b2443209f9e65dbbca87
Salvatore Del Vecchio <96501610+saldelv@users.noreply.github.com>
2024-06-03 16:49:52 -0400

    Fixed prepare_dataset

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,6 +1,9 @@
 import os
 import csv
 from PIL import Image
+from PIL import ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 dir = 'data'
 if not os.path.exists('data_merged'):
@@ -14,13 +17,12 @@ for fname in os.listdir(dir):
     path = os.path.join(dir, fname)
     if os.path.isdir(path):
         for filename in os.listdir(path):
-
             # Turns pngs to 8 bit depth for pytorch
-            image = Image.open(path + "/" + filename)
+            image = Image.open(path + "/" + filename).convert('RGB')
             if image.format == 'PNG':
                 image = image.quantize(colors=256, method=2)
+            print(filename)
             image.save(targetdir + "/" + filename)
-
             # Stores filenames with indexes for csv
             rows.append([filename, index])
     index += 1
